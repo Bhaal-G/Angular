9446a7962a6fffbf40c517ba49be24c039d28bfc
Nipurn <nbhaal0805@gmail.com>
2026-06-23 02:31:51 +0530

    startFromScratchForRouting

--- src/app/pipe/convert-pipe-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConvertPipePipe } from './convert-pipe-pipe';
-
-describe('ConvertPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ConvertPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
