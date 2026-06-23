ec6452b137b47cd1bf85f21148ab607add31de6c
Nipurn <nbhaal0805@gmail.com>
2026-06-23 22:02:47 +0530

    LazyLoading

--- RoutingAndApi/src/app/app.routes.ts
+++ RoutingAndApi/src/app/app.routes.ts
@@ -2,8 +2,29 @@ import { Routes } from '@angular/router';
 import { HomeComp } from './home-comp/home-comp';
 import { ContactComp } from './contact-comp/contact-comp';
 import { AboutComp } from './about-comp/about-comp';
+import { Dashboard } from './dashboard/dashboard';
+import { Profile } from './dashboard/profile/profile';
+import { Setting } from './dashboard/setting/setting';
 export const routes: Routes = [
-    {path:'',component:HomeComp},
-    {path:'about',component:AboutComp},
-    {path:'contact',component:ContactComp}
+    // {path:'',component:HomeComp},
+    // {path:'about',component:AboutComp},
+    // {path:'contact',component:ContactComp}
+
+    // {
+    //     path:'dashboard',
+    //     component:Dashboard,  
+    //     children:[
+    //         {path:'profile',component:Profile},
+    //         {path:'setting',component:Setting}        ]
+    // },
+    // {path:'',redirectTo:'dashboard',pathMatch:'full'},
+
+    {path:'home',component:HomeComp},
+    {
+        path:'admin',
+        loadComponent:()=>import('./admin/admin').then(m=>m.Admin)
+    },
+    {path:'',redirectTo:'home',pathMatch:'full'}
+
+
 ];
