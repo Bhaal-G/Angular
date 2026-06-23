7ae5bd58fe2ade2067a6238dfac1f06ddf0df61b
Nipurn <nbhaal0805@gmail.com>
2026-06-23 20:09:11 +0530

    Basics of Routing

--- RoutingAndApi/src/app/app.routes.ts
+++ RoutingAndApi/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComp } from './home-comp/home-comp';
+import { ContactComp } from './contact-comp/contact-comp';
+import { AboutComp } from './about-comp/about-comp';
+export const routes: Routes = [
+    {path:'',component:HomeComp},
+    {path:'about',component:AboutComp},
+    {path:'contact',component:ContactComp}
+];
